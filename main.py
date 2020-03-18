34d7c56380d2d48dabda55ebde89383d933d3a81
Dmitry <l.dimka@gmail.com>
2020-03-19 00:31:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,37 +6,44 @@ import csv
 import os
 
 start_time = datetime.now()
+n = 0
 with open('list.csv', newline='') as File:
+
     reader = csv.reader(File, delimiter=';', quotechar=',',
-                        quoting=csv.QUOTE_ALL)
+                            quoting=csv.QUOTE_ALL)
     fileend = 'end.csv'
-    if os.path.isfile(fileend):
-        os.remove(fileend)
+        #if os.path.isfile(fileend):
+            #os.remove(fileend)
 
     for row in reader:
-        try:
-            url = row[1]
-        except:
-            url=''
+        n += 1
+        if n > 8787:
+            try:
+                url = row[1]
+            except:
+                url = ''
 
-        if 'volvo' not in url:
-            print('not url')
-        else:
-            page = requests.get(url)
-            soup = BeautifulSoup(page.text, "html.parser")
-            ostat = soup.find('div', class_='product-table__qty')
-            if ostat:
-                ostat = ostat.text
-                ostat = re.findall('(\d+)', ostat)
-                if not ostat:
-                    print('под заказ')
-                else:
-                    ostat = [row[0], ostat[0]]
-                    print(ostat)
-                    with open(fileend, 'a', newline='') as filecsv:
-                        writer = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
-                        writer.writerow(ostat)
+            if 'volvo' not in url:
+                print('not url')
             else:
-                print('error find class')
+                page = requests.get(url)
+                if page.status_code != 200:
+                    print('URL ERROR: ' + page.status+ ' ('+row[0]+')')
+                else:
+                    soup = BeautifulSoup(page.text, "html.parser")
+                    ostat = soup.find('div', class_='product-table__qty')
+                    if ostat:
+                        ostat = ostat.text
+                        ostat = re.findall('(\d+)', ostat)
+                        if not ostat:
+                            print('под заказ')
+                        else:
+                            ostat = [row[0], ostat[0]]
+                            print(ostat)
+                            with open(fileend, 'a', newline='') as filecsv:
+                                writer = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
+                                writer.writerow(ostat)
+                    else:
+                        print('error find class')
 
 print(datetime.now() - start_time)
