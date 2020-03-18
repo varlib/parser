6bbee5b2df65dd3b6550b01c55c72b8ef03a8f46
Dmitry <l.dimka@gmail.com>
2020-03-18 14:53:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,15 +25,18 @@ with open('list.csv', newline='') as File:
             page = requests.get(url)
             soup = BeautifulSoup(page.text, "html.parser")
             ostat = soup.find('div', class_='product-table__qty')
-            ostat = ostat.text
-            ostat = re.findall('(\d+)', ostat)
-            if not ostat:
-                print('под заказ')
+            if ostat:
+                ostat = ostat.text
+                ostat = re.findall('(\d+)', ostat)
+                if not ostat:
+                    print('под заказ')
+                else:
+                    ostat = [row[0], ostat[0]]
+                    print(ostat)
+                    with open(fileend, 'a', newline='') as filecsv:
+                        writer = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
+                        writer.writerow(ostat)
             else:
-                ostat = [row[0], ostat[0]]
-                print(ostat)
-                with open(fileend, 'a', newline='') as filecsv:
-                    writer = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
-                    writer.writerow(ostat)
+                print('error find class')
 
 print(datetime.now() - start_time)
