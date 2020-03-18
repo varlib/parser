e8c62bb6fd844f8ce745dff52fdcb2bd8133105a
Dmitry <l.dimka@gmail.com>
2020-03-18 12:00:25 +0300

    main

--- main.py
+++ main.py
@@ -2,28 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import csv
+import os
 
 with open('list.csv', newline='') as File:
     reader = csv.reader(File, delimiter=';', quotechar=',',
-                        quoting=csv.QUOTE_MINIMAL)
+                        quoting=csv.QUOTE_ALL)
+    fileend = 'end.csv'
+    if os.path.isfile(fileend):
+        os.remove('end.csv')
+
     for row in reader:
-        #print(row[1])
-        #url = 'https://volvogroupshop.ru/parts/section/346947/part/548241/?case=detail&part_number=VO20535535'
         url = row[1]
-        page = requests.get(url)
-
-        soup = BeautifulSoup(page.text, "html.parser")
-
-        ostat = soup.find('div', class_='product-table__qty')
-
-        ostat = ostat.text
-        ostat = re.findall('(\d+)', ostat)
-
-
-        if not ostat:
-            print('под заказ')
+        if 'volvo' not in url:
+            print('URL: ' + url)
         else:
-            print(ostat)
-            with open('end.csv', 'a', newline='') as filecsv:
-                writer = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
-                writer.writerow(map(int, ostat))
+            page = requests.get(url)
+
+            soup = BeautifulSoup(page.text, "html.parser")
+
+            ostat = soup.find('div', class_='product-table__qty')
+
+            try:
+                ostat = ostat.text
+                ostat = re.findall('(\d+)', ostat)
+
+                ostat = [row[0], ostat[0]]
+
+                if not ostat:
+                    print('под заказ')
+                else:
+                    print(ostat)
+                    with open(fileend, 'a', newline='') as filecsv:
+                        writer = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
+                        writer.writerow(ostat)
+            except:
+                print('ERROR: '+row[0])
